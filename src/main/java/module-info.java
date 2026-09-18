c812c679ea9ce44e2f1d972dd0b81ff7484e0456
Daniel Pascal O. <boogeyclub@gmail.com>
2026-09-18 17:30:51 +0100

    inital_commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module cm.odigital.intracompany {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens cm.odigital.intracompany to javafx.fxml;
+    exports cm.odigital.intracompany;
+}
