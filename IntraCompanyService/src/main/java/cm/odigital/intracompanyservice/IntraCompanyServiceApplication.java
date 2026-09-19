aa0ea2bb0655115617a24ebfebb1f2b059d4417b
Daniel Pascal O. <boogeyclub@gmail.com>
2026-09-19 10:47:11 +0100

    update project repository

---
+++ IntraCompanyService/src/main/java/cm/odigital/intracompanyservice/IntraCompanyServiceApplication.java
@@ -0,0 +1,13 @@
+package cm.odigital.intracompanyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntraCompanyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntraCompanyServiceApplication.class, args);
+	}
+
+}
---
+++ IntraCompanyService/src/test/java/cm/odigital/intracompanyservice/IntraCompanyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.odigital.intracompanyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntraCompanyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
